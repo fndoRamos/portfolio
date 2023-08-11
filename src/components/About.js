8fc1ffb87226112081860816e9aa2165183f2f0c
Fernando Ramos <fndo.ramos@gmail.com>
2023-08-11 13:07:35 +0200

    add about me style and animations and start projects section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React from "react";
+// countup
+import CountUp from "react-countup";
+// Intersection observer hook
+import { useInView } from "react-intersection-observer";
+// motion
+import { motion } from "framer-motion";
+//variant
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col h-screen gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0">
+          {/* img */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-contain bg-about bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
+          {/* text */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="mb-4 h3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ab dolor cupiditate asperiores ipsam.
+            </h3>
+            <p className="mb-6">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Doloribus a illum facilis, maxime ipsum commodi laborum
+              exercitationem animi neque sapiente pariatur aliquam
+              necessitatibus! Consequuntur odio magnam voluptas a accusamus.
+              Cum.
+            </p>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React from "react";
+// motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
+//images
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Projects = () => {
-  return <div className='section' id='projects'>Projects</div>;
+  return (
+    <section className="section" id="projects">
+      <div className="container mx-auto">
+        <div>
+          <div className="flex flex-col flex-1 mb-10 gap-y-12 lg:mb-0">
+            {/* text */}
+            <div>
+              <h2 className="leading-tight h2 text-accent">
+                My Latest <br />
+                Work.
+              </h2>
+              <p className="max-w-sm mb-16">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut est
+                error a veritatis fugiat ipsa dicta et vel deserunt, nam
+                cupiditate hic quibusdam ad perferendis distinctio eius libero
+                tempora laboriosam.
+              </p>
+              <button className="btn btn-sm">View all projects</button>
+            </div>
+            {/* image */}
+            <div className="relative overflow-hidden border-2 group border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="absolute z-40 w-full h-full transition-all duration-300 group-hover:bg-black/70"></div>
+              {/* img */}
+              <img
+                className="transition-all duration-500 group-hover:scale-125"
+                src={Img1}
+                alt="project1"
+              />
+              {/* pretitle */}
+              <div className="absolute z-50 transition-all duration-500 -bottom-full left-12 group-hover:bottom-24">
+                <span className="text-gradient">WEB DEV</span>
+              </div>
+              {/* title */}
+              <div className="absolute z-50 transition-all duration-700 -bottom-full left-12 group-hover:bottom-14">
+                <span className="text-white text-3x1">Project title</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
